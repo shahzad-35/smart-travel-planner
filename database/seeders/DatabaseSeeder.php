73e20a649a0c0ad706f53925b4bb86263f59bf24
Shahzad Ali <shahzad.ali@manafatech.com>
2025-10-02 17:57:00 +0500

    livewire setup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\UserPreference;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +14,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create()->each(function ($user) {
+            UserPreference::factory()->create([
+                'user_id' => $user->id,
+            ]);
+        });
 
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-        ]);
+        ])->each(function ($user) {
+            UserPreference::factory()->create([
+                'user_id' => $user->id,
+            ]);
+        });
     }
 }
